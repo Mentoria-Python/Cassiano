4b36b4fb0a57c6699a1bc17e1152deaa42ef9a64
cassiano <cassiandrei@gmail.com>
2023-03-06 20:38:06 -0300

    Ex2 - modulo 2

---
+++ modulo_2/ex2.py
@@ -0,0 +1,25 @@
+a = "A4-Verde-Comercial Trifásica 220/127"
+b = "A4-Verde-Rural Bifásica 380/220"
+c = "B3-Industrial Monofásica 220"
+
+print(c[-1])
+
+a = a.split(' ')
+b = b.split(' ')
+c = c.split(' ')
+print(f'{a[1]} {a[2]}')
+print(f'{b[1]} {b[2]}')
+print(f'{c[1]} {c[2]}')
+
+
+# texto = "5-Monofásica-120V"
+# print(texto)
+# texto = texto.split("-")
+# print(type(texto))
+# print(texto)
+#
+# print(texto[0])
+# print(texto[1])
+# print(texto[2])
+
+
